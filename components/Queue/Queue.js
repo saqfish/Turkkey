ab4d613a7c6784f45edfbb27c9aee60f81f0c772
saqfish <saqfish@gmail.com>
2020-04-11 22:27:54 -0500

    Queue UI/fetch

--- components/Queue/Queue.js
+++ components/Queue/Queue.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import {View} from 'react-native';
+import {ActivityIndicator, Colors} from 'react-native-paper';
+
 import QueueTable from './QueueTable';
 
-const Queue = () => {
+const Queue = props => {
+  console.log('Queue');
+  const {navigation} = props;
+  const [queueData, setQueueData] = useState();
+  const getQueueData = () => {
+    let url = 'https://worker.mturk.com/tasks.json';
+    fetch(`${url}`, {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        setQueueData(res.tasks);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    navigation.addListener('focus', () => {
+      console.log('fetching...');
+      getQueueData();
+    });
+  }, [navigation]);
+
+  useEffect(() => {
+    console.log(queueData);
+  }, [queueData]);
+
   return (
     <View>
-      <QueueTable />
+      {typeof queueData === 'undefined' ? (
+        <ActivityIndicator animating={true} color={Colors.red800} />
+      ) : (
+        <QueueTable data={queueData} />
+      )}
     </View>
   );
 };
--- components/Queue/QueueTable.js
+++ components/Queue/QueueTable.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import {DataTable} from 'react-native-paper';
 
-const QueueTable = () => {
-  return (
-    <DataTable>
-      <DataTable.Row>
-        <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-        <DataTable.Cell numeric>159</DataTable.Cell>
-        <DataTable.Cell numeric>6.0</DataTable.Cell>
-      </DataTable.Row>
-
-      <DataTable.Row>
-        <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-        <DataTable.Cell numeric>237</DataTable.Cell>
-        <DataTable.Cell numeric>8.0</DataTable.Cell>
-      </DataTable.Row>
-
-      <DataTable.Pagination
-        page={1}
-        numberOfPages={3}
-        onPageChange={page => {
-          console.log(page);
-        }}
-        label="1-2 of 6"
-      />
-    </DataTable>
-  );
+const QueueTable = props => {
+  const bank = props.data;
+  return bank.map(task => {
+    const hit = task.project;
+    return (
+      <DataTable>
+        <DataTable.Row>
+          <DataTable.Cell>{hit.requester_name}</DataTable.Cell>
+          <DataTable.Cell numeric>{hit.title}</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+    );
+  });
 };
 
 export default QueueTable;
