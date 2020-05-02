b0974ae72098751385e06fd41d07c16dc2fd529a
saqfish <saqfish@gmail.com>
2020-05-02 01:16:15 -0500

    Refactor

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,10 +1,5 @@
 import {createContext} from 'react';
 
 export const snackBarContext = createContext({});
-export const scrapeContext = createContext({
-  rate: 1,
-  reward: 0,
-  qualified: false,
-  masters: false,
-});
+export const scrapeContext = createContext({});
 export const settingsContext = createContext({});
