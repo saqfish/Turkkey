1b717e4d13ea4138d5d86071674b23e708d2d799
saqfish <saqfish@gmail.com>
2020-05-05 01:28:01 -0500

    UI - Hit styles

--- src/components/Scrape/Hit.js
+++ src/components/Scrape/Hit.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Card, Text, Subheading, Divider} from 'react-native-paper';
+import {
+  IconButton,
+  Card,
+  Text,
+  Subheading,
+  Divider,
+  withTheme,
+} from 'react-native-paper';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Hit = React.memo(props => {
-  const {hit, navigation} = props;
+  const {hit, navigation, theme} = props;
   const getPayColor = pay => {
     return pay > 4 ? 'green' : pay > 3 ? 'yellow' : pay > 2 ? 'orange' : 'red';
   };
@@ -30,20 +37,35 @@ const Hit = React.memo(props => {
   };
 
   const styles = StyleSheet.create({
-    cardRight: {
-      marginVertical: 0,
+    cardStyle: {
       paddingRight: 10,
+      paddingLeft: 10,
+      paddingBottom: 0,
+      justifyContent: 'flex-start',
     },
-    cardContent: {
-      marginVertical: 0,
+    cardRight: {
+      justifyContent: 'flex-start',
+    },
+    cardLeft: {
+      justifyContent: 'flex-end',
+      height: 20,
+      width: 20,
     },
     cardTitle: {
-      marginBottom: 0,
       marginVertical: 0,
+      marginBottom: 2,
     },
-    labels: {
+    cardSubtitle: {
+      justifyContent: 'flex-start',
+      marginBottom: 0,
+    },
+    cardContent: {
       flexDirection: 'row',
       marginVertical: 0,
+      paddingHorizontal: 10,
+      paddingVertical: 0,
+      paddingTop: 0,
+      paddingBottom: 10,
     },
     moneyLabel: {
       fontSize: 16,
@@ -75,11 +97,38 @@ const Hit = React.memo(props => {
               <Subheading>{hit.title}</Subheading>
             </>
           }
-          right={() => <Subheading> {hit.assignable_hits_count}</Subheading>}
+          right={inProps => (
+            <IconButton
+              {...inProps}
+              icon="chevron-right"
+              onPress={() => {
+                const uri = `https://worker.mturk.com/${
+                  hit.accept_project_task_url
+                }`;
+                navigation.navigate('WebView', {
+                  uri,
+                });
+              }}
+            />
+          )}
+          left={() => (
+            <Icon
+              name={
+                hit.assignable_hits_count > 1
+                  ? 'file-document-box-multiple-outline'
+                  : 'file-document-box-outline'
+              }
+              size={20}
+              color={theme.colors.text}
+            />
+          )}
+          style={styles.cardStyle}
+          subtitleStyle={styles.cardSubtitle}
           rightStyle={styles.cardRight}
+          leftStyle={styles.cardLeft}
           titleStyle={styles.cardTitle}
         />
-        <Card.Content style={styles.labels}>
+        <Card.Content style={styles.cardContent}>
           <Subheading style={styles.moneyLabel}>
             <Icon name="currency-usd" color={'green'} size={16} />
             {hit.monetary_reward.amount_in_dollars.toFixed(2)}
@@ -94,4 +143,4 @@ const Hit = React.memo(props => {
   );
 });
 
-export default Hit;
+export default withTheme(Hit);
